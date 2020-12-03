8a145626c27ef419e77456a8b82b2538e130fb9a
jansona <jan_sona@outlook.com>
2020-12-03 14:33:06 +0800

    fix: 取消生成部分的注释

--- predict.py
+++ predict.py
@@ -95,38 +95,38 @@ if not opt.engine and not opt.onnx:
 else:
     from run_engine import run_trt_engine, run_onnx
     
-# start_time = time.time()
-# for i, data in enumerate(dataset):
-#     if i >= opt.how_many:
-#         break
-#     if opt.data_type == 16:
-#         data['label'] = data['label'].half()
-#         data['inst']  = data['inst'].half()
-#     elif opt.data_type == 8:
-#         data['label'] = data['label'].uint8()
-#         data['inst']  = data['inst'].uint8()
-#     if opt.export_onnx:
-#         print ("Exporting to ONNX: ", opt.export_onnx)
-#         assert opt.export_onnx.endswith("onnx"), "Export model file should end with .onnx"
-#         torch.onnx.export(model, [data['label'], data['inst']],
-#                           opt.export_onnx, verbose=True)
-#         exit(0)
-#     minibatch = 1 
-#     if opt.engine:
-#         generated = run_trt_engine(opt.engine, minibatch, [data['label'], data['inst']])
-#     elif opt.onnx:
-#         generated = run_onnx(opt.onnx, opt.data_type, minibatch, [data['label'], data['inst']])
-#     else:        
-#         generated = model.inference(data['label'], data['inst'], data['image'])
+start_time = time.time()
+for i, data in enumerate(dataset):
+    if i >= opt.how_many:
+        break
+    if opt.data_type == 16:
+        data['label'] = data['label'].half()
+        data['inst']  = data['inst'].half()
+    elif opt.data_type == 8:
+        data['label'] = data['label'].uint8()
+        data['inst']  = data['inst'].uint8()
+    if opt.export_onnx:
+        print ("Exporting to ONNX: ", opt.export_onnx)
+        assert opt.export_onnx.endswith("onnx"), "Export model file should end with .onnx"
+        torch.onnx.export(model, [data['label'], data['inst']],
+                          opt.export_onnx, verbose=True)
+        exit(0)
+    minibatch = 1 
+    if opt.engine:
+        generated = run_trt_engine(opt.engine, minibatch, [data['label'], data['inst']])
+    elif opt.onnx:
+        generated = run_onnx(opt.onnx, opt.data_type, minibatch, [data['label'], data['inst']])
+    else:        
+        generated = model.inference(data['label'], data['inst'], data['image'])
         
-#     visuals = OrderedDict([('', util.tensor2im(generated.data[0]))])
-#     img_path = data['path']
-#     print('process image... %s' % img_path)
-#     visualizer.save_images_predict(webpage, visuals, img_path)
-# end_time = time.time()
-# print(end_time - start_time, "seconds")
+    visuals = OrderedDict([('', util.tensor2im(generated.data[0]))])
+    img_path = data['path']
+    print('process image... %s' % img_path)
+    visualizer.save_images_predict(webpage, visuals, img_path)
+end_time = time.time()
+print(end_time - start_time, "seconds")
 
-# webpage.save()
+webpage.save()
 
 # automatic integrate and autocontrast - for platform
 print("start integrating...")
